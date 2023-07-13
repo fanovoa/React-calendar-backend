6388223194ef7fa242bebac9aaf98c3da3a83a7e
fanovoa <fabian93novoa@gmail.com>
2023-07-13 16:34:37 -0500

    manejar middleware para los errores - uso de express-validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,15 +5,6 @@ const crearUsuario = (req= request  , res = response ) =>{
     
     const { name, email, password } =req.body;
 
-    //Manejo de errores
-    const errors = validationResult( req );
-    if(!errors.isEmpty() ) {
-        return res.status(400).json({
-            ok:false,
-            errors: errors.mapped()
-        });
-    }
-
     res.status(201).json({
         ok:true,
         msg:'registro',
@@ -28,16 +19,7 @@ const loginUsuario = (req= request  , res = response ) =>{
 
 
     const {  email, password } =req.body;
-    const errors = validationResult( req );
-
-    if(!errors.isEmpty() ) {
-        return res.status(400).json({
-            ok:false,
-            errors: errors.mapped()
-        });
-    }
     
-
     res.json({
         ok:true,
         msg:'login',
--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,10 @@
 
 const { Router }= require('express');
 const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos')
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 const router = Router();
 
-const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
 
 router.post('/new',
@@ -15,6 +16,7 @@ router.post('/new',
     check("name", "El nombre es obligatorio").not().isEmpty(),
     check("email","El email es obligatorio").isEmail(),
     check("password", "El password debe de ser de seis caracteres").isLength({min:6}),
+    validarCampos
 
 ],
 crearUsuario);
@@ -23,6 +25,7 @@ router.post('/',
 [
     check("email","El email es obligatorio").isEmail(),
     check("password", "El password debe de ser de seis caracteres").isLength({min:6}),
+    validarCampos
 ],
  loginUsuario);
 
