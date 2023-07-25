67c8ffcbe0c4d44d49db4a867e4d8304a6e01ea1
fanovoa <fabian93novoa@gmail.com>
2023-07-24 19:14:22 -0500

    path * agregado

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+app.get('*' , ( req,res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 
 
 
